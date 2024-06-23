65e9caaf0b6dd90eda9fdd95384da8970304b70c
Blossom87 <yoannbeune87@gmail.com>
2024-06-23 21:11:04 +0200

    Update

--- src/main/java/fr/afpa/employees/Employee.java
+++ src/main/java/fr/afpa/employees/Employee.java
@@ -1,5 +1,8 @@
 package fr.afpa.employees;
 
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+
 /*
 Objectif :
 L’objectif de ce TP est de concevoir un programme en console basé sur une approche objet et
@@ -9,31 +12,107 @@ Vous allez écrire une classe représentant les salariés d'une entreprise.
 Vous pourrez vous aider du PDF disponible sur pour obtenir des informations sur la façon de faire.
 */
 
-class Employee
-{
+class Employee {
 	/**
 	 * Matricule de l'employé
+	 * Permet de stock les informations a rentré
 	 */
 	private String registrationNumber;
+	private String lastName;
+	private String firstName;
+	private double salary;
+	private LocalDate birthDate;
+	private final int socialRate = 30;
 
-	// TODO compléter les attributs comme présenté dans le PDF
+	// Permet d'attribué les données de la fonction aux variables dessous (les
+	// variables sont instancies afin d'être initialiser dans un autre code)
 
-	// TODO compléter le constructeur de la classe
-	public Employee(String registrationNumber) {
+	public Employee(String registrationNumber, String lastName, String firstName, double salary, String birthDate) {
 		this.registrationNumber = registrationNumber;
+		this.lastName = lastName;
+		this.firstName = firstName;
+		this.salary = salary;
+		this.birthDate = LocalDate.parse(birthDate);
 	}
 
-	// TODO implémenter les setters et getters de la classe (permet d'accéder aux attributs privés)
+	// GETTERS : Permet de récupéréss les données d'un objet.
+
 	public String getRegistrationNumber() {
 		return registrationNumber;
 	}
 
-	public void setRegistrationNumber(String registrationNumber) throws Exception {
+	public String getLastName() {
+		return lastName;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public double getSalary() {
+		return salary;
+	}
+
+	public LocalDate getBirthdatDate() {
+		return birthDate;
+	}
+
+	public void setRegistrationNumber(String registrationNumber) {
 		this.registrationNumber = registrationNumber;
 	}
 
-	// TODO ajouter les getters et les setters
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
 
-	// TODO implémenter la méthode "toString()" qui renvoie une chaîne de caractère qui représente un objet de la classe employé
-	// plus d'information sur la méthode "toString()" -> https://codegym.cc/fr/groups/posts/fr.986.mthode-java-tostring
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public void setSalary(double salary) {
+		this.salary = salary;
+	}
+
+	public void setLocalDate(LocalDate birthDate) {
+		this.birthDate = birthDate;
+	}
+
+	@Override
+
+	public String toString() {
+
+		return "Employee {"
+				+ " \nMatricule " + getRegistrationNumber()
+				+ " \nNom " + getLastName()
+				+ " \nPrénom " + getFirstName()
+				+ " \nSalaire " + getSalary()
+				+ " \nBirthday " + getBirthdatDate()
+				+ " \nDays before Birthday " + daysBeforeBirthdate()
+				+ " \nSalary " + netSalary()
+				+ '}';
+	}
+
+	public double netSalary() {
+
+		return this.salary - this.salary * (this.socialRate / 100.0);
+	}
+
+	public long daysBeforeBirthdate() {
+		LocalDate currentDate = LocalDate.now();
+		LocalDate nextBirthDate = this.birthDate.withYear(currentDate.getYear());//.withYear permet de set l'année de birthyear sur l'année actuelle > Prochain Anniversaire
+
+		// Si l'anniversaire de cette année est déjà passé, on prend l'année prochaine
+		if (nextBirthDate.isBefore(currentDate) || nextBirthDate.isEqual(currentDate)){
+			nextBirthDate = nextBirthDate.plusYears(1); // .plusYear rajoute une valeur à l'année, pareil avec .plusMonths / .plusDays
+		}
+
+		return ChronoUnit.DAYS.between(currentDate, nextBirthDate);
+	}
+
+
+
+	// TODO implémenter la méthode "toString()" qui renvoie une chaîne de caractère
+	// qui représente un objet de la classe employé
+	// plus d'information sur la méthode "toString()" ->
+	// https://codegym.cc/fr/groups/posts/fr.986.mthode-java-tostring
 }
--- src/main/java/fr/afpa/employees/EmployeeMain.java
+++ src/main/java/fr/afpa/employees/EmployeeMain.java
@@ -6,16 +6,25 @@ import java.time.temporal.ChronoUnit;
 /**
  * Classe principale du projet, contient la fonction "main"
  */
-class EmployeeMain
-{
-	public static void main(String[] args) 
-	{
+class EmployeeMain {
+	public static void main(String[] args) {
 		System.out.println("\n----- Exercice de programmation objet - classe \"Employee\" -----");
 
-		// TODO instanciez 4 objets de la classe "Employee" (une fois celle-ci implémentée)
-		Employee employee1 = new Employee("11ABC22");
+		// TODO instanciez 4 objets de la classe "Employee" (une fois celle-ci
+		// implémentée)
+		Employee employee1 = new Employee("111aaa12", "Shazam", "Samantha", 1800.0, "1994-12-12");
+
+		// Appelle de methode a partir de l'objet employee1 de la classe Employee (le
+		// "." demande accès à une donnée spécifique ici registrationNumber)
+		System.out.println(employee1.getRegistrationNumber());
+
+		// Modification de la variable registrationNumber graçe au Setter de la classe
+		// Employee, nouvelle donnée attribuer a registrationNumber
+		employee1.setRegistrationNumber("22ZZZ33");
+		System.out.println(employee1.getRegistrationNumber());
+
+		System.out.println(employee1.toString());
 
 		// TODO afficher les informations des employés avec System.out.println
 	}
 }
-
